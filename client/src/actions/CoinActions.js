cca3c69c87f7f3b33635d199136027208f20494c
Patrick Klima <patrick.klima@gmail.com>
2018-03-10 22:46:01 -0600

    Adds actions folder and basic CoinActions file

---
+++ client/src/actions/CoinActions.js
@@ -0,0 +1,25 @@
+export const GET_COIN_INDEX_REQUEST = "GET_COIN_INDEX_REQUEST";
+export const GET_COIN_INDEX_SUCCESS = "GET_COIN_INDEX_SUCCESS";
+export const GET_COIN_INDEX_FAILURE = "GET_COIN_INDEX_FAILURE";
+
+export const getCoinIndexRequest = () => {
+  return {
+    type: GET_COIN_INDEX_REQUEST,
+  };
+}
+export const getCoinIndexSuccess = (data) => {
+  return {
+    type: GET_COIN_INDEX_SUCCESS,
+    data: data
+  };
+}
+export const getCoinIndexFailure = (error) => {
+  return {
+    type: GET_COIN_INDEX_FAILURE,
+    data: error
+  };
+}
+export const getCoinIndex = () => {
+  //TO DO
+  return;
+}
