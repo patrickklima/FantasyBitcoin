3b9063894e7199ba9beee5b84dfc31cdd87a47dc
Aaron Saloff <aaronsaloff@gmail.com>
2018-03-13 05:45:44 -0400

    changed coinList url

--- client/src/actions/CoinActions.js
+++ client/src/actions/CoinActions.js
@@ -2,23 +2,27 @@ export const GET_COIN_INDEX_REQUEST = "GET_COIN_INDEX_REQUEST";
 export const GET_COIN_INDEX_SUCCESS = "GET_COIN_INDEX_SUCCESS";
 export const GET_COIN_INDEX_FAILURE = "GET_COIN_INDEX_FAILURE";
 
-const apiCoinListUrl = 'https://www.cryptocompare.com/api/data/coinlist/';
+const apiCoinListUrl = 'https://min-api.cryptocompare.com/data/all/coinlist';
 
 export const getCoinIndex = () => {
   return (dispatch) => {
     dispatch(getCoinIndexRequest());
     fetch(apiCoinListUrl, {
-      method: 'GET', 
-      mode: 'cors',
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      cors: true
     })
-    .then(res => {
-      if (!res.ok) {
-        throw new Error(`${res.statusText}: ${res.error}`)
-      }
-      return res.json();
-    })
-    .then(json => dispatch(getCoinIndexSuccess(json)))
-    .catch(error => dispatch(getCoinIndexFailure(error)));
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(`${res.statusText}: ${res.error}`);
+        }
+        return res.json();
+      })
+      .then(json => dispatch(getCoinIndexSuccess(json)))
+      .catch(error => dispatch(getCoinIndexFailure(error)));
   };
 };
 
