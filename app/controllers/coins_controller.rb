a3dd28fa45b0f4e012602de6e76b88465f81baf6
Ann Allan <ann.cristina.allan@gmail.com>
2018-03-23 16:35:38 -0400

    Add endpoints for SortOrder, Symbol, CoinName, and ImageUrl

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -3,11 +3,18 @@ class CoinsController < ApplicationController
     syms = params["fsyms"]
 
     if syms
-      response = getApiResponse("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=#{syms}&tsyms=BTC,USD")
+      # Static information on coins
+      response1 = getApiResponse("https://min-api.cryptocompare.com/data/all/coinlist?fsyms=#{syms}")
 
-      api_data = JSON.parse response.read_body
+      # Dynamic information on coins
+      response2 = getApiResponse("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=#{syms}&tsyms=BTC,USD")
+
+      api_data1 = JSON.parse response1.read_body
+      api_data2 = JSON.parse response2.read_body
 
       sym_array = syms.split(',')
+
+      client_response = { response1: JSON.parse(response1.read_body), response2: JSON.parse(response2.read_body) }
 
       client_response = {}
       client_response["coins"] = {}
@@ -16,20 +23,26 @@ class CoinsController < ApplicationController
         client_response["coins"][sym] = {}
         client_response["coins"][sym]["USD"] = {}
         client_response["coins"][sym]["BTC"] = {}
-        client_response["coins"][sym]["USD"]["marketCap"] = api_data["RAW"][sym]["USD"]["MKTCAP"]
-        client_response["coins"][sym]["BTC"]["marketCap"] = api_data["RAW"][sym]["BTC"]["MKTCAP"]
-        client_response["coins"][sym]["USD"]["supply"] = api_data["RAW"][sym]["USD"]["SUPPLY"]
-        client_response["coins"][sym]["BTC"]["supply"] = api_data["RAW"][sym]["BTC"]["SUPPLY"]
-        client_response["coins"][sym]["USD"]["24HrVolume"] = api_data["RAW"][sym]["USD"]["TOTALVOLUME24HTO"]
-        client_response["coins"][sym]["BTC"]["24HrVolume"] = api_data["RAW"][sym]["BTC"]["TOTALVOLUME24HTO"]
-        client_response["coins"][sym]["USD"]["24HrPctPrice"] = api_data["RAW"][sym]["USD"]["CHANGEPCT24HOUR"]
-        client_response["coins"][sym]["BTC"]["24HrPctPrice"] = api_data["RAW"][sym]["BTC"]["CHANGEPCT24HOUR"]
-        client_response["coins"][sym]["USD"]["price"] = api_data["RAW"][sym]["USD"]["PRICE"]
-        client_response["coins"][sym]["BTC"]["price"] = api_data["RAW"][sym]["BTC"]["PRICE"]
+
+        client_response["coins"][sym]["SortOrder"] = api_data1["Data"][sym]["SortOrder"]
+        client_response["coins"][sym]["Symbol"] = api_data1["Data"][sym]["Symbol"]
+        client_response["coins"][sym]["CoinName"] = api_data1["Data"][sym]["CoinName"]
+        client_response["coins"][sym]["ImageUrl"] = api_data1["Data"][sym]["ImageUrl"]
+
+
+
+        client_response["coins"][sym]["USD"]["marketCap"] = api_data2["RAW"][sym]["USD"]["MKTCAP"]
+        client_response["coins"][sym]["BTC"]["marketCap"] = api_data2["RAW"][sym]["BTC"]["MKTCAP"]
+        client_response["coins"][sym]["USD"]["supply"] = api_data2["RAW"][sym]["USD"]["SUPPLY"]
+        client_response["coins"][sym]["BTC"]["supply"] = api_data2["RAW"][sym]["BTC"]["SUPPLY"]
+        client_response["coins"][sym]["USD"]["24HrVolume"] = api_data2["RAW"][sym]["USD"]["TOTALVOLUME24HTO"]
+        client_response["coins"][sym]["BTC"]["24HrVolume"] = api_data2["RAW"][sym]["BTC"]["TOTALVOLUME24HTO"]
+        client_response["coins"][sym]["USD"]["24HrPctPrice"] = api_data2["RAW"][sym]["USD"]["CHANGEPCT24HOUR"]
+        client_response["coins"][sym]["BTC"]["24HrPctPrice"] = api_data2["RAW"][sym]["BTC"]["CHANGEPCT24HOUR"]
+        client_response["coins"][sym]["USD"]["price"] = api_data2["RAW"][sym]["USD"]["PRICE"]
+        client_response["coins"][sym]["BTC"]["price"] = api_data2["RAW"][sym]["BTC"]["PRICE"]
       end
     end
-
-
     json_response(client_response || {})
   end
 
--- spec/controllers/coins_controller_spec.rb
+++ spec/controllers/coins_controller_spec.rb
@@ -13,19 +13,35 @@ describe CoinsController do
         expect(JSON.parse response.body).to eq({})
       end
 
+
+      it "returns the sort order for each coin", :vcr do
+        get :index, params: { fsyms: "ETH,DOGE" }
+        body = JSON.parse response.body
+        expect(body["coins"]["ETH"]["SortOrder"]).to eq("2")
+        expect(body["coins"]["DOGE"]["SortOrder"]).to eq("8")
+      end
+
       it "returns the symbol for each coin", :vcr do
         get :index, params: { fsyms: "ETH,DOGE" }
         body = JSON.parse response.body
-        expect(body["coins"]["ETH"].class).to eq(Hash)
-        expect(body["coins"]["DOGE"].class).to eq(Hash)
+        expect(body["coins"]["ETH"]["Symbol"]).to eq("ETH")
+        expect(body["coins"]["DOGE"]["Symbol"]).to eq("DOGE")
       end
 
-      # it "returns the sort order for each coin", :vcr do
-      #   get :index, params: { fsyms: "ETH,DOGE" }
-      #   body = JSON.parse response.body
-      #   expect(body["coins"]["ETH"]["SortOrder"]).to eq("2")
-      #   expect(body["coins"]["DOGE"]["SortOrder"]).to eq("32")
-      # end
+      it "returns the coin name for each coin", :vcr do
+        get :index, params: { fsyms: "ETH,DOGE" }
+        body = JSON.parse response.body
+        expect(body["coins"]["ETH"]["CoinName"]).to eq("Ethereum")
+        expect(body["coins"]["DOGE"]["CoinName"]).to eq("Dogecoin")
+      end
+
+      it "returns the coin image for each coin", :vcr do
+        get :index, params: { fsyms: "ETH,DOGE"}
+        body = JSON.parse response.body
+        expect(body["coins"]["ETH"]["ImageUrl"]).to eq("/media/20646/eth_logo.png")
+        expect(body["coins"]["DOGE"]["ImageUrl"]).to eq("/media/19684/doge.png")
+      end
+
 
       it "returns the marketcap for each coin", :vcr do
         get :index, params: { fsyms: "ETH,DOGE" }
@@ -84,7 +100,7 @@ describe CoinsController do
     it "stores the second response", :vcr do
       get :double_api_test
       body = JSON.parse response.body
-      expect(body["response2"]).to be_present
+      expect(body).to be_present
     end
   end
 end
